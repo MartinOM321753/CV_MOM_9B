2e5ff6610ff4df7bc2f5107195812c74a7203607
MartinOM321753 <133397788+MartinOM321753@users.noreply.github.com>
2025-05-29 10:35:20 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/mx/edu/utez/cv_mom_9b/CvMom9BApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cv_mom_9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvMom9BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvMom9BApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/utez/cv_mom_9b/CvMom9BApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cv_mom_9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvMom9BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
